3053e27129445d5bd25430e2ea54400571c42fb9
baliramakrishna <2016uec1342@mnit.ac.in>
2019-04-11 17:26:21 +0530

    back end of face recognition app

---
+++ controllers/profile.js
@@ -0,0 +1,28 @@
+
+
+
+const handleProfile=(req,res)=>{
+
+
+ const {id}=req.params;
+ db.select('*').from('users').where({id}).
+ then(user=>{
+
+ if(user.length){
+      res.json(user[0])
+    }
+      else{
+     res.status(400).json('NOT FOUND');
+      }
+    
+ }).catch(err=> res.status(400).json('error getting user'));
+   
+
+}
+
+
+
+module.exports={
+
+	handleProfile:handleProfile
+}
---
+++ server.js
@@ -0,0 +1,43 @@
+
+const express=require('express');
+const bodyParser=require('body-parser');
+const cors=require('cors')
+const bcrypt=require('bcrypt-nodejs');
+const knex=require('knex');
+const register=require('./controllers/register');
+const signin=require('./controllers/signin');
+const profile=require('./controllers/profile');
+const image=require('./controllers/image');
+
+const db=knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'postgres',
+    password : '5411',
+    database : 'raki'
+  }
+});
+
+
+const app=express();
+app.use(cors());
+app.use(bodyParser.json());
+
+
+
+
+app.post('/signin', (req,res)=>{ signin.handleSignin(req,res,db,bcrypt); } )
+
+app.post('/register',(req,res)=>{  register.handleRegister(req,res,db,bcrypt); } )
+
+app.get('/profile/:id',(req,res)=>{  profile.handleProfile(req,res,db); } )
+
+app.put('/image',(req,res)=>{  image.handleImage(req,res,db); } )
+app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
+
+
+app.listen(process.env.PORT || 3000,()=>{
+
+	console.log(`app is running on port ${process.env.PORT}`);
+});
